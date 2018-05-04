ca326646db314debe7147e859231fd276b6a804d
Taylor Williams <taylor@thewilliams.net>
2018-05-04 22:20:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |combo|
     if position_taken?(board,combo[0])
       if board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]]
-        combo
+        return combo
       end
     end
   end
